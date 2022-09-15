3f601286a5f8d73aa0793cf738d3afe68cb8bbce
raulindo <raulindo.neto@usp.br>
2022-09-15 17:17:40 -0300

    update code

--- Tensions/plane_varying_direction.m
+++ Tensions/plane_varying_direction.m
@@ -6,14 +6,14 @@
 
 xx = -20 ; xy = 0 ; xz = 0;
 yx = xy ; yy = 20 ; yz = 00;
-zx = xz ; zy = yz ; zz = 20;
+zx = xz ; zy = yz ; zz = 0;
 
 stress_matrix = [xx xy xz;
                  yx yy yz;
                  zx zy zz];
              
 % dipdir = 135;
-dip = 30;
+dip = 90;
 
 dd = linspace(0,360,361);
 i = 1;
@@ -30,26 +30,27 @@ while i<=361
     cossdir2line(s);
     
     
-    % tensão normal sobre o plano
-    % calcula a magntude da tensão normal sobre o plano
-    mag_sn = abs(dot(s,pole)); 
-    % a orientação do vetor normal sobre o plano é o próprio polo multiplicado pela magnitude;
+    % tensao normal sobre o plano
+    % calcula a magntude da tensao normal sobre o plano
+    mag_sn = dot(s,pole); 
+    % a orientacao do vetor normal sobre o plano e o próprio polo multiplicado pela magnitude;
     snvetor = pole*mag_sn;
-    fprintf('O vetor de esforço normal [%.2f;%0.2f;%0.2f]\n', snvetor(1),snvetor(2),snvetor(3));
-    % printa a magnitude do vetor esforço normal
-    fprintf('A magnitude do vetor esforço normal sobre o plano é %.2f MPa\n', mag_sn);
-    % transforma o vetor tensão normal ao plano em atitude novamente
-    cossdir2line(snvetor); % essa é a linha do polo do plano
+    fprintf('O vetor de esforco normal [%.2f;%0.2f;%0.2f]\n', snvetor(1),snvetor(2),snvetor(3));
+    % printa a magnitude do vetor esforco normal
+    fprintf('A magnitude do vetor esforco normal sobre o plano e %.2f MPa\n', mag_sn);
+    % transforma o vetor tensao normal ao plano em atitude novamente
+    cossdir2line(snvetor); % essa e a linha do polo do plano
     
-    % tensão cisalhante sobre o plano
+    % esforco cisalhante sobre o plano
     ssvetor = s - snvetor;
-    % a magnitude do vetor esforço cisalhante é S - esforço normal
-    mag_ss = norm(s)-mag_sn;
-    % os componentes do vetor cisalhante são os componentes do vetor total s -
-    % os componentes do vetor tensão normal
-    fprintf('O vetor de esforço cisalhante [%.2f;%0.2f;%0.2f]\n', ssvetor(1), ssvetor(2), ssvetor(3));
-    % printa a magnitude do vetor esforço cisalhante
-    fprintf('A magnitude do vetor esforço cisalhante sobre o plano é %.2f Mpa\n', mag_ss);
+    % a magnitude do vetor esforco cisalhante é S - esforco normal
+    mag_ss = sqrt(ssvetor(1)^2+ssvetor(2)^2 + ssvetor(3)^2);
+    % os componentes do vetor cisalhante sao os componentes do vetor total s -
+    % os componentes do vetor tensao normal
+    fprintf('O vetor de esforco cisalhante [%.2f;%0.2f;%0.2f]\n', ssvetor(1), ssvetor(2), ssvetor(3));
+    % printa a magnitude do vetor esforco cisalhante
+    fprintf('A magnitude do vetor esforco cisalhante sobre o plano e %.2f Mpa\n', mag_ss);
+
     % transforma o vetor esforço cisalhante ao plano em atitude novamente
     cossdir2line(ssvetor); % essa é uma linha contida no plano
     % calcular o rake da tensão cisalhante com a linha
@@ -79,8 +80,8 @@ hold on
 
 plot(dd, shear_mag, 'linewidth', 1, 'color','r')
 
-legend({'Normal Stress \sigma_n','Shear Stress \sigma_s'},'Location','northeast')
-title('EE=-20Mpa; NN=20Mpa; ZZ=20Mpa;  Strike variando de 0-360')
+legend({'Normal Stress \sigma_n','Shear Stress \sigma_s'},'Location','southeast')
+title('EE=-20Mpa; NN=20Mpa e demais componentes iguais a zero;  Strike variando de 0-360')
 xlabel('dip direction')
 ylabel('\sigma (MPa)')
 xticks(linspace(0,360,9))
